735d405876f76941a92886b580174a473caed5ca
Elizabeth3852 <omelnitskaya.liza@mail.ru>
2016-03-08 08:14:51 -0600

    Button_1

--- ВП_Лабораторная1/ВП_Лабораторная1/Form2.cs
+++ ВП_Лабораторная1/ВП_Лабораторная1/Form2.cs
@@ -16,5 +16,21 @@ namespace ВП_Лабораторная1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            char[] obrtext = text.ToCharArray();
+            Array.Reverse(obrtext); // метод поворота символов введённых данных в обратном порядке
+            string finaltext = new string(obrtext); // строка с инициализацией символов из массива
+            if (text == finaltext) // сравнение исходной строки и перевернутой строкой
+            {
+                MessageBox.Show(" \n\t Данная запись является палиндромом \n\t\t ");
+            }
+            if (text != finaltext)
+            {
+                MessageBox.Show(" \n\t Данная запись не является палиндромом \n\t\t ");
+            }
+        }
     }
 }
